3c47bbd568b9752fe6d6f5fa73dd74f304f9ad53
Sterling <verbal@setbit.org>
2015-10-27 19:58:43 -0500

    Changed switch statements to more concise array-hashes

--- myclass.php
+++ myclass.php
@@ -1,20 +1,23 @@
 <?php
+/*   myclass.php
+     ~/.composer/laravel# php artisan tinker
+     require_once "myclass.php";
+  
+  public static function first_day_of( $month , $year)
+  public static function prefill( $month , $year)
+  public static function show_cal( $month , $year )
+  public static function postfill( $month , $year)
+  public static function last_day_of( $month , $year)
+  public static function days_in_month( $month , $year )
+  public static function make_cal_array( $month , $year )
+  public static function is_leap_year( $year )
+*/
+
 class Mycal
 {
-  // The start of a web calendar in PHP
-  // todo:  add some handy unit tests
-  // 2015-10-14 Wednesday. First commit
-  //
-  public static function first_day_of( $month, $year)
+  public static function first_day_of( $month , $year)
   {
-    /* 
-    root@laravel5:~/.composer/laravel# php artisan tinker
-    Psy Shell v0.5.1 (PHP 5.5.9-1ubuntu4.11 — cli) by Justin Hileman
-    >>> require_once  "myclass.php";
-    => 1
-    >>> Mycal::days_in_month(3,2008);
-    => 31
-  
+    /*
     >>> Mycal::first_day_of(10,2015);
     => "Thu"
     >>> Mycal::first_day_of(7,1974);
@@ -27,50 +30,177 @@ class Mycal
     => "error"
     */
 
-    // make sure input is an integer 
-    if ( ! ( is_integer($month) ) or ( ! ( is_integer($year) ) ) ) 
+    // make sure input is an integer
+    if ( ! ( is_integer($month) ) or ( ! ( is_integer($year) ) ) )
     {
       return "error";
-    } 
+    }
 
-    // make sure month is in range  
+    // make sure month is in range
     if ( ( $month < 1 ) or ( $month > 12 ) )
     {
       return "error";
     }
 
-    $d = date('w', strtotime('1-'. $month . '-'. $year ));
-    switch ( $d ) {
-      case "0":
-        return "Sun";
-        break;
-      case "1":
-        return "Mon";
-        break;
-      case "2":
-        return "Tue";
-        break;
-      case "3":
-        return "Wed";
-        break;
-      case "4":
-        return "Thu";
-        break;
-      case "5":
-        return "Fri";
-        break;
-      case "6":
-        return "Sat";
-        break;
-      default:
-        return "error";
-        break;
-  }
+    $d = date('w' , strtotime('1-'. $month . '-'. $year ));
+    $day_array = [
+                    0 => "Sun" ,
+                    1 => "Mon" ,
+                    2 => "Tue" ,
+                    3 => "Wed" ,
+                    4 => "Thu" ,
+                    5 => "Fri" ,
+                    6 => "Sat"
+                ];
+
+    return $day_array[$d];
 }
 
-  public static function days_in_month( $month, $year )
+  public static function prefill( $month , $year)
   {
-    /* 
+    /*
+      returns integer -> number of days needed
+      to be added to the front of a 7-day calendar period
+      to make a full 7-day period 
+      
+       >>> Mycal::prefill(10,2015);
+       => 4
+       >>> Mycal::prefill(11,2015);
+       => 0
+       >>> Mycal::prefill(1,1974);
+       => 2
+       >>> Mycal::prefill(1.5,1974);
+       => "error"
+    */
+
+    // make sure input is an integer
+    if ( ! ( is_integer($month) ) or ( ! ( is_integer($year) ) ) )
+    {
+      return "error";
+    }
+
+    // make sure month is in range
+    if ( ( $month < 1 ) or ( $month > 12 ) )
+    {
+      return "error";
+    }
+
+    $f = Mycal::first_day_of( $month , $year);
+    $day_array = [
+                    "Sun" => 0 ,
+                    "Mon" => 1 ,
+                    "Tue" => 2 ,
+                    "Wed" => 3 ,
+                    "Thu" => 4 ,
+                    "Fri" => 5 ,
+                    "Sat" => 6
+                ];
+
+    return $day_array[$f];
+  }
+
+  public static function show_cal( $month , $year )
+  {
+    $g = Mycal::make_cal_array( $month , $year );
+    for ( $a = 0  ; $a <  7; $a++ ){  print $g[$a] . " "; }
+    print "\n";
+    for ( $a = 7  ; $a < 14; $a++ ){  print $g[$a] . " "; }
+    print "\n";
+    for ( $a = 14 ; $a < 21; $a++ ){  print $g[$a] . " "; }
+    print "\n";
+    for ( $a = 21 ; $a < 28; $a++ ){  print $g[$a] . " "; }
+    print "\n";
+    for ( $a = 28 ; $a < 35; $a++ ){  print $g[$a] . " "; }
+    print "\n";
+  }
+
+  public static function postfill( $month , $year )
+  {
+    /*
+      returns integer -> number of days needed
+      to round-out a full 7-day calendar period
+
+       >>> Mycal::postfill(1,1974);
+       => 2
+       >>> Mycal::postfill(5,1974);
+       => 1
+       >>> Mycal::postfill(8,1974);
+       => 0
+       >>> Mycal::postfill(9,2015);
+       => 3
+   */
+    // make sure input is an integer
+    if ( ! ( is_integer($month) ) or ( ! ( is_integer($year) ) ) )
+    {
+      return "error";
+    }
+
+    // make sure month is in range
+    if ( ( $month < 1 ) or ( $month > 12 ) )
+    {
+      return "error";
+    }
+
+    $f = Mycal::last_day_of( $month , $year);
+
+    $day_array = [
+                    "Sun" => 6 ,
+                    "Mon" => 5 ,
+                    "Tue" => 4 ,
+                    "Wed" => 3 ,
+                    "Thu" => 2 ,
+                    "Fri" => 1 ,
+                    "Sat" => 0
+                ];
+
+    return $day_array[$f];
+  }
+
+  public static function last_day_of( $month , $year)
+  {
+    /*
+     >>> Mycal::last_day_of(10,2015);
+     => "Sat"
+     >>> Mycal::last_day_of(12,2015);
+     => "Thu"
+     >>> Mycal::last_day_of(2,1974);
+     => "Thu"
+     >>> Mycal::last_day_of(8,1974);
+     => "Sat"
+     >>> Mycal::last_day_of(8.8,1974);
+     => "error"
+    */
+
+    // make sure input is an integer
+    if ( ! ( is_integer($month) ) or ( ! ( is_integer($year) ) ) )
+    {
+      return "error";
+    }
+
+    // make sure month is in range
+    if ( ( $month < 1 ) or ( $month > 12 ) )
+    {
+      return "error";
+    }
+
+    $dimz = Mycal::days_in_month( $month , $year);
+    $d = date('w' , strtotime( $dimz . '-' . $month . '-'. $year ) );
+
+    $day_array = [
+                    "0" => "Sun" ,
+                    "1" => "Mon" ,
+                    "2" => "Tue" ,
+                    "3" => "Wed" ,
+                    "4" => "Thu" ,
+                    "5" => "Fri" ,
+                    "6" => "Sat"
+                ];
+     return $day_array[$d];  // todo
+  }
+
+  public static function days_in_month( $month , $year )
+  {
+    /*
     >>> Mycal::days_in_month(2,2008);
     => 29
     >>> Mycal::days_in_month(2,1964);
@@ -85,44 +215,85 @@ class Mycal
     => 31
     */
 
-    if ( ! ( is_integer($month) ) or ( ! ( is_integer($year) ) ) ) 
+    if ( ! ( is_integer($month) ) or ( ! ( is_integer($year) ) ) )
     {
       return "error";
-    } 
+    }
 
-    // make sure month is in range  
+    // make sure month is in range
     if ( ( $month < 1 ) or ( $month > 12 ) )
     {
       return "error";
     }
 
-    return cal_days_in_month( CAL_GREGORIAN, $month, $year );
+    return cal_days_in_month( CAL_GREGORIAN , $month , $year );
   }
-  
+
+  public static function make_cal_array( $month , $year )
+  {
+    $too = Mycal::days_in_month( $month , $year );
+    $body = range( 1, $too );
+
+    $hd = Mycal::prefill( $month , $year );
+    $tl = Mycal::postfill( $month , $year );
+
+    if ( ( $hd == 0 ) and ( $tl == 0 ) )
+    {
+        $all = $body;
+        return $all;
+    }
+
+    if ( ( $hd > 0 ) and ( $tl > 0 ) )
+    {
+      $front = array_fill(0 , $hd , "--");
+      $back  = array_fill(0 , $tl , "++");
+
+      $all = array_merge( $front ,  $body , $back );
+
+      return  $all;
+    }
+
+    if ( ( $hd ==  0 )  and ( $tl > 0 ) )
+    {
+      // $front = array_fill(0 , $hd , "--");
+      $back  = array_fill(0 , $tl , "++");
+      $all = array_merge( $body , $back );
+
+      return $all;
+    }
+
+    if ( ( $hd > 0 )  and ( $tl == 0 ) )
+    {
+      $front = array_fill(0 , $hd , "--");
+      $all = array_merge( $front , $body );
+
+      return $all;
+    }
+  }
+
   public static function is_leap_year( $year )
   {
     /*
-    >>> Mycal::is_leap_year(1972);
-    => true
-    >>> Mycal::is_leap_year(1988);
-    => true
-    >>> Mycal::is_leap_year(1990);
-    => false
-    >>> Mycal::is_leap_year(2002);
-    => false
+      >>> Mycal::is_leap_year(1972);
+      => true
+      >>> Mycal::is_leap_year(1988);
+      => true
+      >>> Mycal::is_leap_year(1990);
+      => false
+      >>> Mycal::is_leap_year(2002);
+      => false
     */
 
-    if ( ! ( is_integer($year) ) ) 
+    if ( ! ( is_integer($year) ) )
     {
       return "error";
-    } 
+    }
 
-    if (cal_days_in_month( CAL_GREGORIAN, 2, $year) == 29 )
+    if ( cal_days_in_month( CAL_GREGORIAN, 2, $year ) == 29 )
     {
       return true;
     } else {
       return false;
     }
   }
-
 }
